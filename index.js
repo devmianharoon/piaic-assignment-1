f7ad407a3a2651c8c9d1cdc10366cda7c7e1e413
devmianharoon <devmianharoon@gmail.com>
2023-07-31 22:19:10 +0500

    ts assignment 1

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+// Task 1
+console.log("Hey Mian Haroon");
+// Task 2
+var userName = "Mian Haroon";
+console.log(userName);
+// Task3
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var num6 = 60;
+var num7 = 70;
+var num8 = 80;
+var num9 = 90;
+var num10 = 100;
+var sumResult = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log(sumResult);
+// Task4
+var subResult = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log(subResult);
+// Task5
+var mulResult = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log(mulResult);
+// Task 6
+var divResult = num8 / num10;
+console.log(divResult);
---
+++ index.ts
@@ -0,0 +1,32 @@
+// Task 1
+console.log("Hey Mian Haroon");
+// Task 2
+var userName:string = "Mian Haroon"
+console.log(userName);
+// Task3
+var num1:number= 10;
+var num2:number=20
+var num3:number=30
+var num4:number=40
+var num5:number=50
+var num6:number=60
+var num7:number=70
+var num8:number=80
+var num9:number=90
+var num10:number=100
+var sumResult= num1+num2+num3+num4+num5+num6+num7+num8+num9+num10
+console.log( sumResult);
+// Task4
+var subResult= num1-num2-num3-num4-num5-num6-num7-num8-num9-num10
+console.log(subResult);
+// Task5
+var mulResult= num1*num2*num3*num4*num5*num6*num7*num8*num9*num10
+console.log(mulResult);
+// Task 6
+var divResult=num8/num10
+console.log(divResult);
+
+
+
+
+
